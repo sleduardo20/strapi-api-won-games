ad1db0fbe183e777749edcd7fe4cc6fb3d34acbe
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-19 14:07:40 -0300

    chore: create first service

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,8 +5,15 @@
  * to customize this service
  */
 
-module.exports = {
-  populate: (params) => {
-    console.log("Serviço Populate");
+const axios = require("axios");
+
+ module.exports = {
+  populate: async (params) => {
+   const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+
+   const {data: {products} } = await axios.get(gogApiUrl)
+
+   console.log(products)
   }
 };
+
