87a4127f38017ce80edd68dbe3718c5539d5ad78
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-19 06:05:35 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '51ef5cd2f36251828360a4972fb68a68'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b1562e04-7468-4bf5-a69f-20c5328cc6b6'
+};
