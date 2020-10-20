66ac7e2f8dcf491d2a6b1614a4d763bd96f3fccb
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-19 22:14:01 -0300

    chore: create service populate

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,13 +7,33 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug){
+  const jsdom = require("jsdom");
+
+  const {JSDOM} = jsdom;
+
+  const response = await axios.get(`https://www.gog.com/game/${slug}`);
+
+  const dom = new JSDOM(response.data);
+
+  const description = dom.window.document.querySelector('.description');
+
+  return {
+    rating: 'Free',
+    short_description: description.textContent.trim().slice(0,160),
+    description: description.innerHTML,
+  }
+}
+
  module.exports = {
   populate: async (params) => {
    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
-   const {data: {products} } = await axios.get(gogApiUrl)
+   const {data: {products} } = await axios.get(gogApiUrl);
 
-   console.log(products[0])
+   console.log(products[6]);
+
+   console.log(await getGameInfo(products[13].slug));
   }
 };
 
