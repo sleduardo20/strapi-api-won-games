da361beba4e65aeffefef053ce4d49cb12ae415d
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-19 21:23:33 -0300

    🎨 styles: prettier code

--- config/functions/cart.js
+++ config/functions/cart.js
@@ -1,40 +1,37 @@
 const cartGamesIds = async (cart) => {
-  return await cart.map( (game) => ({
+  return await cart.map((game) => ({
     id: game.id,
-  }))
+  }));
 };
 
-const cartItems = async ( cart ) => {
+const cartItems = async (cart) => {
   let games = [];
 
   await Promise.all(
-    cart?.map( async (game) => {
+    cart?.map(async (game) => {
       const validatedGame = await strapi.services.game.findOne({
-        id: game.id
-      })
+        id: game.id,
+      });
 
-      if(validatedGame) {
+      if (validatedGame) {
         games.push(validatedGame);
       }
     })
   );
 
   return games;
-
 };
 
-const total = async ( games ) => {
-
-  const ammout = await games.reduce((acc, game) => {
+const total = async (games) => {
+  const amount = await games.reduce((acc, game) => {
     return acc + game.price;
   }, 0);
 
-  return Number((ammout * 100).toFixed(0));
+  return Number((amount * 100).toFixed(0));
 };
 
-
 module.exports = {
+  cartGamesIds,
   cartItems,
   total,
-  cartGamesIds
-}
+};
